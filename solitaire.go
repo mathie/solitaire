e3b0b2f6355b8d662f34a365ac0caba424489bdd
Graeme Mathieson <mathie@woss.name>
2014-12-09 11:00:03 +0000

    First stab an an encryption routine.

--- solitaire.go
+++ solitaire.go
@@ -4,6 +4,30 @@ import (
   "fmt"
 )
 
+func Encrypt(message, keystream []string) []string {
+  encodedMessage := StreamEncoder(message)
+  encodedKeystream := StreamEncoder(keystream)
+
+  results := [][]int{}
+
+  for i := 0; i < len(encodedMessage); i++ {
+    messageBlock := encodedMessage[i]
+    keystreamBlock := encodedKeystream[i]
+    resultsBlock := []int{}
+
+    for j := 0; j < len(messageBlock); j++ {
+      messageChar   := messageBlock[j]
+      keystreamChar := keystreamBlock[j]
+      result := (messageChar + keystreamChar) % 26
+      resultsBlock = append(resultsBlock, result)
+    }
+
+    results = append(results, resultsBlock)
+  }
+
+  return StreamDecoder(results)
+}
+
 func main() {
   fmt.Println("Hello World.")
 }
--- solitaire_test.go
+++ solitaire_test.go
@@ -1,5 +1,17 @@
 package main
 
+import "testing"
+
+func TestEncryption(t *testing.T) {
+  message   := []string{ "CODEI", "NRUBY", "LIVEL", "ONGER" }
+  keystream := []string{ "DWJXH", "YRFDG", "TMSHP", "UURXJ" }
+  expected  := []string{ "GLNCQ", "MJAFF", "FVOMB", "JIYCB" }
+
+  actual := Encrypt(message, keystream)
+
+  assertSameStringArray(expected, actual, t)
+}
+
 func ExampleHelloWorld() {
   main()
 
